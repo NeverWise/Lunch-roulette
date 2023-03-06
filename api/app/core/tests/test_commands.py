4fffe3f7ad6cb89cf37a762fc363f35805bbccc8
NeverWise <6242691+NeverWise@users.noreply.github.com>
2023-03-06 23:11:40 +0100

    Removed useless comments and downgrade Django version to fix a bug of wait_for_db function.

--- api/app/core/tests/test_commands.py
+++ api/app/core/tests/test_commands.py
@@ -2,7 +2,6 @@
 Test custom Django management commands.
 """
 
-# Due possibili eccezioni che si possono ottenere.
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 
@@ -15,8 +14,6 @@ from django.test import SimpleTestCase
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
-    # Si imposta il risultato del metodo check uguale a True e ci si
-    # aspetta che venga chiamato una sola volta.
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
@@ -25,8 +22,6 @@ class CommandTests(SimpleTestCase):
 
         patched_check.assert_called_once_with(databases=['default'])
 
-    # Si impostano una serie di eccezioni come risultato del metodo check
-    # e ci si aspetta che venga chiamato 6 volte.
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
