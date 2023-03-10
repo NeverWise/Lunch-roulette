0cddcd7786a6bfab8349976a6e2689729871f7c2
NeverWise <6242691+NeverWise@users.noreply.github.com>
2023-03-11 00:02:48 +0100

    Added cron service and improved Django settings

--- api/app/config/settings.py
+++ api/app/config/settings.py
@@ -21,13 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fhdavfx+vv575=$98&7#q2y)o1y$#adazk1x1+uq^j)kh@6p5_'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 # Application definition
 
